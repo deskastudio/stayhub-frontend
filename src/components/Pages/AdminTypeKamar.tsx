0b6370db608b388c30753b84e7d914eb335c6d14
danendradipa <danendrach@gmail.com>
2024-11-02 11:07:02 +0700

    add AdminTypeKamar

---
+++ src/components/Pages/AdminTypeKamar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TypeKamar: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Type Kamar</h1>
+            <p>Manage Type Kamar.</p>
+        </div>
+    );
+};
+
+export default TypeKamar;
