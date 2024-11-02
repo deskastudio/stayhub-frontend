ebe5edb7f0b7be3e9967f4c83fde36c5afcca9cc
danendradipa <danendrach@gmail.com>
2024-11-02 23:21:11 +0700

    feat: add AdminTypeKamar component pages

--- src/components/Pages/AdminTypeKamar.tsx
+++ src/components/Pages/AdminTypeKamar.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ProfileInfo from "../Elements/ProfileInfo";
+import TypeKamarTable from "../Elements/TypeKamarTable";
+import AdminTypeKamarTambah from "./AdminTypeKamarTambah";
+import AdminTypeKamarDetail from "./AdminTypeKamarDetail";
+import AdminTypeKamarEdit from "./AdminTypeKamarEdit";
 
 const TypeKamar: React.FC = () => {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Type Kamar</h1>
-            <p>Manage Type Kamar.</p>
+  const location = useLocation();
+  
+  let pageTitle = "Type Kamar";
+  if (location.pathname === "/type-kamar/tambah") {
+    pageTitle = "Tambah Type Kamar";
+  } else if (location.pathname.startsWith("/type-kamar/detail")) {
+    pageTitle = "Detail Type Kamar";
+  } else if (location.pathname.startsWith("/type-kamar/edit")) {
+    pageTitle = "Edit Type Kamar";
+  }
+  console.log("Current Path:", location.pathname);
+
+  return (
+    <MainLayout>
+      <div className="p-8 flex-grow">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold">{pageTitle}</h1>
+          <ProfileInfo />
         </div>
-    );
+        <Routes>
+          <Route path="/" element={<TypeKamarTable />} />
+          <Route path="tambah" element={<AdminTypeKamarTambah />} />
+          <Route path="detail/:id" element={<AdminTypeKamarDetail />} />
+          <Route path="edit/:id" element={<AdminTypeKamarEdit />} />
+        </Routes>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default TypeKamar;
