be2900fd76a75bbb0322d662077f4be0e1683319
danendradipa <danendrach@gmail.com>
2024-11-10 09:21:53 +0700

    update button component

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -3,12 +3,22 @@ import React from "react";
 interface ButtonProps {
   custom: string;
   children: React.ReactNode;
-  type?: "button" | "submit" | "reset"; 
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ custom, children, type = "button" }) => { 
+const Button: React.FC<ButtonProps> = ({
+  custom,
+  children,
+  type = "button",
+  onClick,
+}) => {
   return (
-    <button type={type} className={`px-7 font-semibold font-main rounded-lg ${custom}`}>
+    <button
+      type={type}
+      onClick={onClick} // Pastikan onClick disertakan di sini
+      className={`px-7 font-semibold font-main rounded-lg ${custom}`}
+    >
       {children}
     </button>
   );
