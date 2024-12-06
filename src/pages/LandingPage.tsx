848cf3d4b43a4ce11490b544243afa8cfd6225aa
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-06 21:41:23 +0800

    update routes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Navbar from '../components/Layouts/Navbar'
-import Hero from '../components/Layouts/Hero'
-import About from '../components/Layouts/About'
-import Facilities from '../components/Layouts/Facilities'
-import FAQList from '../components/Layouts/FAQList'
-import Room from '../components/Layouts/Room'
-import Testimonial from '../components/Layouts/Testimonial'
-import Footer from '../components/Layouts/Footer'
+import React from 'react';
+import Navbar from '../components/Layouts/Navbar';
+import Hero from '../components/Layouts/Hero';
+import About from '../components/Layouts/About';
+import Facilities from '../components/Layouts/Facilities';
+import FAQList from '../components/Layouts/FAQList';
+import TypeRoom from '../components/Layouts/Room';
+import Testimonial from '../components/Layouts/Testimonial';
+import Footer from '../components/Layouts/Footer';
 
-const LandingPage:React.FC = () => {
-
+const LandingPage: React.FC = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
-      <Facilities/>
+      <Facilities />
       <FAQList />
-      <Room/>
-      <Testimonial/>
-      <Footer/>
+      <TypeRoom />
+      <Testimonial />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
