a5e0a0c409898c5efa97892b43c3f55853b1ab92
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-08 19:25:48 +0800

    fetch room data

--- src/Pages/UserBeranda.tsx
+++ src/Pages/UserBeranda.tsx
@@ -1,25 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import Button from '../components/Elements/Button';
 import ProfileUser from '../components/Fragments/ProfileUser';
 import BookingModal from '../components/Fragments/BookingModal';
+import { format, add } from 'date-fns';
+import { id } from 'date-fns/locale';
+import { getUserName, getRoomId } from '../utils/auth.utils';
 
-// new import axios and use authUtils
-import { getUserName } from '../utils/auth.utils';
+// Interfaces
+interface Room {
+  name: string;
+  updatedAt: string;
+}
+
+interface RoomType {
+  name: string;
+  cost: number;
+}
 
 const UserBeranda: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [room, setRoom] = useState<Room | null>(null);
+  const [type, setType] = useState<RoomType | null>(null);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   // NEW get token and idUser
   const userName = getUserName();
+  const token = sessionStorage.getItem('token');
+  const roomId = getRoomId();
+
+  useEffect(() => {
+    const fetchRoom = async () => {
+      try {
+        // Fetch data room
+        const response = await axios.get(
+          `http://localhost:8000/room/${roomId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        // Set data room
+        const roomData = response.data.data;
+        if (roomData) {
+          const room = roomData;
+          const type = room.type[0];
+
+          setRoom({
+            name: room.name,
+            updatedAt: room.updatedAt,
+          });
+
+          setType({
+            name: type.name,
+            cost: type.cost,
+          });
+        }
+      } catch (error) {
+        if (error) {
+          // Alert error
+          setTimeout(() => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Anda belum menyelesaikan pembayaran',
+              showConfirmButton: false,
+              timer: 2000,
+            });
+          }, 1000);
+        }
+      }
+    };
+
+    fetchRoom();
+  }, [token, roomId]);
 
   return (
     <div className='p-8 flex-grow'>
       <div className='flex justify-between items-start mb-6'>
         <div className='flex flex-col gap-3'>
-          <h1 className='text-2xl font-bold'>Hello {userName} 👋</h1>
+          <h1 className='text-2xl font-bold'>Halo {userName} 👋</h1>
           <p>Yuk cek tagihan kamar kosmu sekarang juga</p>
         </div>
         <ProfileUser />
@@ -33,7 +98,13 @@ const UserBeranda: React.FC = () => {
       <div className='overflow-x-auto w-full'>
         <div className='flex space-x-4'>
           <div className='bg-white p-4 rounded-lg flex-1 space-y-5'>
-            <p className='font-semibold'>Tagihan bulan ini: Oktober</p>
+            <p className='font-semibold'>Tagihan bulan ini</p>
+            <span>
+              {room?.updatedAt &&
+                format(new Date(room.updatedAt), ' MMMM yyyy', {
+                  locale: id,
+                })}
+            </span>
             <div className='flex gap-5 items-center'>
               <p className='text-xl font-bold'>Belum Lunas</p>
               <img src='icon/cross-red.svg' alt='' />
@@ -44,18 +115,64 @@ const UserBeranda: React.FC = () => {
               Kamu belum membayar tagihan bulan ini
             </p>
             <div className='flex gap-5 items-center'>
-              <p className='text-xl font-bold'>Rp 900.000</p>
+              <p className='text-xl font-bold'>
+                Rp.{' '}
+                {(type?.cost &&
+                  new Intl.NumberFormat('id-ID').format(type.cost)) ||
+                  0}
+              </p>
               <img src='icon/money.svg' alt='' />
             </div>
           </div>
         </div>
         <div className='bg-white p-4 rounded-lg mt-6'>
-          <p className='font-semibold text-lg'>Detail Penyewaan:</p>
-          <p className='mt-2'>Nomor Kamar : GOLD-001</p>
-          <p className='mt-2'>Tipe Kamar : Gold</p>
-          <p className='mt-2'>Tarif Kamar: Rp 900.000</p>
-          <p className='mt-2'>Tanggal Penyewaan: 2024-10-01</p>
-          <p className='mt-2'>Tanggal Akhir Sewa: 2024-11-01</p>
+          <p className='font-semibold text-lg'>Detail Penyewaan</p>
+          <table className='mt-4 text-left'>
+            <tbody>
+              <tr>
+                <th className='px-4 py-2 text-gray-700'>Nomor Kamar</th>
+                <th>:</th>
+                <td className='px-4 py-2'>{room?.name}</td>
+              </tr>
+              <tr>
+                <th className='px-4 py-2 text-gray-700'>Tipe Kamar</th>
+                <th>:</th>
+                <td className='px-4 py-2'>{type?.name}</td>
+              </tr>
+              <tr>
+                <th className='px-4 py-2 text-gray-700'>Tarif Kamar</th>
+                <th>:</th>
+                <td className='px-4 py-2'>
+                  Rp.{' '}
+                  {(type?.cost &&
+                    new Intl.NumberFormat('id-ID').format(type.cost)) ||
+                    0}
+                </td>
+              </tr>
+              <tr>
+                <th className='px-4 py-2 text-gray-700'>Tanggal Sewa</th>
+                <th>:</th>
+                <td className='px-4 py-2'>
+                  {room?.updatedAt &&
+                    format(new Date(room.updatedAt), 'dd MMMM yyyy', {
+                      locale: id,
+                    })}
+                </td>
+              </tr>
+              <tr>
+                <th className='px-4 py-2 text-gray-700'>Tanggal Akhir Sewa</th>
+                <th>:</th>
+                <td className='px-4 py-2'>
+                  {room?.updatedAt &&
+                    format(
+                      add(new Date(room.updatedAt), { months: 1 }),
+                      'dd MMMM yyyy',
+                      { locale: id }
+                    )}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
