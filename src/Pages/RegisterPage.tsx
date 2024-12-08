a0a24482b847a22f1690eb9080994dad83487509
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-09 03:44:38 +0800

    tidy up code

--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import AuthLayout from '../components/Layouts/AuthLayout';
 import ImageCarousel from '../components/Fragments/ImageCarousel';
 import RegisterForm from '../components/Fragments/RegisterForm';
 
 const RegisterPage: React.FC = () => {
   return (
-    <AuthLayout>
-      <div className="flex h-screen w-full bg-white">
-        <div className="w-full flex items-center justify-center">
-          <div className="w-full max-w-md">
+    <div className='min-h-screen flex justify-center items-center bg-gray-100'>
+      <div className='flex h-screen w-full bg-white'>
+        <div className='w-full flex items-center justify-center'>
+          <div className='w-full max-w-md'>
             <RegisterForm />
           </div>
         </div>
-        <div className="w-full">
+        <div className='w-full'>
           <ImageCarousel />
         </div>
       </div>
-    </AuthLayout>
+    </div>
   );
 };
 
