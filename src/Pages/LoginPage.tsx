d67830b3e4bf69121a0402ce0f3949c2b72043e1
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-08 21:58:06 +0800

    just rename file

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import AuthLayout from '../components/Layouts/AuthLayout';
-import LoginForm from '../components/Fragments/LoginFormUser';
+import LoginForm from '../components/Fragments/LoginForm';
 import ImageCarousel from '../components/Fragments/ImageCarousel';
 
 const LoginPage: React.FC = () => {
   return (
     <AuthLayout>
-      <div className="flex h-screen w-full bg-white">
-        <div className="w-2/3">
+      <div className='flex h-screen w-full bg-white'>
+        <div className='w-2/3'>
           <ImageCarousel />
         </div>
-        <div className="w-full flex items-center justify-center">
-          <div className="w-full max-w-md">
+        <div className='w-full flex items-center justify-center'>
+          <div className='w-full max-w-md'>
             <LoginForm />
           </div>
         </div>
