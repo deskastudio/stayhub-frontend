c1e32b4a94831cf473d70d0a8981bdd93b5e8b03
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-09 15:09:50 +0800

    fix table admin

--- src/components/Elements/DataTableBeranda.tsx
+++ src/components/Elements/DataTableBeranda.tsx
@@ -1,48 +1,71 @@
-// src/components/elements/DataTable.tsx
 import React from 'react';
+import { useFetchRoom } from '../../hooks/useFetchRoom';
+import { format } from 'date-fns';
+import { id } from 'date-fns/locale';
+
+type Room = {
+  name: string;
+  updatedAt: string;
+  type: {
+    cost: number;
+  }[];
+  transaction: {
+    status: string;
+  }[];
+};
 
 const DataTable: React.FC = () => {
-    const data = [
-        { id: 1, tanggal: '21 Okt 2024', penghuni: 'Adzana Ashel', harga: 'Rp. 500.000', status: 'Lunas' },
-        { id: 2, tanggal: '21 Okt 2024', penghuni: 'Zee', harga: 'Rp. 500.000', status: 'Terlambat' },
-    ];
+  const { room } = useFetchRoom();
 
-    return (
-        <div className="bg-white border border-gray-200 rounded-lg shadow-md p-4 w-full">
-            <table className="w-full text-left">
-                <thead>
-                    <tr className="text-gray-500 text-sm border-b">
-                        <th className="py-2">Id</th>
-                        <th className="py-2">Tanggal</th>
-                        <th className="py-2">Penghuni</th>
-                        <th className="py-2">Harga</th>
-                        <th className="py-2">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((item) => (
-                        <tr key={item.id} className="text-gray-700">
-                            <td className="py-2">{item.id}</td>
-                            <td className="py-2">{item.tanggal}</td>
-                            <td className="py-2">{item.penghuni}</td>
-                            <td className="py-2">{item.harga}</td>
-                            <td className="py-2">
-                                <span
-                                    className={`px-2 py-1 rounded-full text-sm ${
-                                        item.status === 'Lunas'
-                                            ? 'bg-green-100 text-green-700'
-                                            : 'bg-red-100 text-red-700'
-                                    }`}
-                                >
-                                    {item.status}
-                                </span>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className='bg-white border border-gray-200 rounded-lg shadow-md p-4 w-full'>
+      <table className='w-full text-left'>
+        <thead>
+          <tr className='text-gray-500 text-sm border-b'>
+            <th className='py-2'>No</th>
+            <th className='py-2'>Tanggal</th>
+            <th className='py-2'>Nomor Kamar</th>
+            <th className='py-2'>Harga</th>
+            <th className='py-2'>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {room?.map((room: Room, index: number) => (
+            <tr className='text-gray-700'>
+              <td className='py-2'>{index + 1}</td>
+              <td className='py-2'>
+                {room?.updatedAt &&
+                  format(new Date(room.updatedAt), 'dd MMMM yyyy', {
+                    locale: id,
+                  })}
+              </td>
+              <td className='py-2'>{room.name}</td>
+              <td className='py-2'>
+                Rp. {room.type[0].cost.toLocaleString('id-ID') || '0'}
+              </td>
+              <td className='py-2'>
+                <span
+                  className={`px-2 py-1 rounded-full text-sm ${
+                    room?.transaction[0]?.status
+                      ? room?.transaction[0]?.status === 'settlement'
+                        ? 'bg-green-100 text-green-700'
+                        : 'bg-yellow-100 text-yellow-700'
+                      : 'bg-red-100 text-red-700'
+                  }`}
+                >
+                  {room?.transaction[0]?.status
+                    ? room?.transaction[0]?.status === 'settlement'
+                      ? 'Lunas'
+                      : 'Belum Bayar'
+                    : 'Ga Laku'}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default DataTable;
