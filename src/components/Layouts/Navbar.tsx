a5bd0320dd2e437a606ae66ae36b14865987bd8d
danendradipa <danendrach@gmail.com>
2024-12-12 21:55:01 +0700

    modify navbar component

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Button from "../Elements/ButtonLanding";
 
@@ -32,6 +32,10 @@ const Navbar: React.FC = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="shadow-md rounded-b-xl sticky top-0 w-full z-10 bg-default">
       <div className="container flex justify-between items-center py-4">
@@ -41,14 +45,15 @@ const Navbar: React.FC = () => {
         <div className="lg:hidden">
           <button
             onClick={toggleMenu}
+            aria-label="Toggle menu"
             className="text-gray-600 text-2xl focus:outline-none"
           >
-            {isMenuOpen ? <FaTimes size="30" color="#113F67"/> : <FaBars size="30" color="#113F67" />}
+            {isMenuOpen ? <FaTimes size="30" color="#113F67" /> : <FaBars size="30" color="#113F67" />}
           </button>
         </div>
 
         {/* Navbar menu */}
-        <div
+        <nav
           className={`${
             isMenuOpen ? "block" : "hidden"
           } lg:block absolute lg:static top-full left-0 w-full rounded-xl lg:w-auto bg-default lg:bg-transparent shadow-md lg:shadow-none`}
@@ -58,6 +63,7 @@ const Navbar: React.FC = () => {
               <li key={item.id}>
                 <a
                   href={item.link}
+                  onClick={closeMenu} // Close menu on click
                   className="block font-main text-gray-400 text-base xl:text-base py-2 lg:py-1 px-2 xl:px-3 hover:text-primary transition-all duration-300 font-medium"
                 >
                   {item.title}
@@ -68,14 +74,14 @@ const Navbar: React.FC = () => {
 
           {/* Buttons */}
           <div className="flex flex-col items-center gap-4 mt-4 pb-6 lg:hidden">
-            <Button custom="border border-primary py-3 text-primary" to="/login">
+            <Button custom="border border-primary py-3 text-primary" to="/login" onClick={closeMenu}>
               Masuk
             </Button>
-            <Button custom="bg-primary py-3 text-white" to="/register">
+            <Button custom="bg-primary py-3 text-white" to="/register" onClick={closeMenu}>
               Daftar
             </Button>
           </div>
-        </div>
+        </nav>
 
         {/* Buttons for large screens */}
         <div className="hidden lg:flex gap-2">
