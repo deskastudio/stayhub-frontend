e668150c504860b510c7fd3bacd12b25736e70dd
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-16 02:38:46 +0800

    check eror code

--- src/components/Layouts/Hero.tsx
+++ src/components/Layouts/Hero.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import Button from "../Elements/ButtonLanding";
+import React from 'react';
+import Button from '../Elements/ButtonLanding';
 
 const Hero: React.FC = () => {
   return (
     <>
-      <div className="container flex py-24 justify-between items-center">
-        <div className="flex flex-col gap-8 max-w-[700px]">
-          <h1 className="text-6xl font-bold text-primary font-main leading-normal">
+      <div className='container flex py-24 justify-between items-center'>
+        <div className='flex flex-col gap-8 max-w-[700px]'>
+          <h1 className='text-6xl font-bold text-primary font-main leading-normal'>
             Temukan Kamar Kos Impian Anda Disini
           </h1>
-          <p className="font-normal font-main text-md text-gray-500 max-w-[530px]">
+          <p className='font-normal font-main text-md text-gray-500 max-w-[530px]'>
             Kami menyediakan layanan lengkap untuk penyewaan kamar kos yang
             nyaman dan sesuai dengan kebutuhan Anda.
           </p>
           <div>
-            <Button custom="bg-primary py-4 text-white">Hubungi Kami</Button>
+            <Button custom='bg-primary py-4 text-white'>Hubungi Kami</Button>
           </div>
         </div>
         <div>
-          <img src="/img-kost.png" alt="" />
+          <img src='/img-kost.png' alt='' />
         </div>
       </div>
     </>
--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 // Definisikan antarmuka untuk props komponen, termasuk properti `children`
 interface MainLayoutProps {
@@ -6,6 +6,10 @@ interface MainLayoutProps {
 }
 
 // Komponen MainLayout hanya menampilkan area konten utama
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => <div className="min-h-screen  bg-primary-custom flex-grow p-6">{children}</div>;
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => (
+  <div className='min-h-screen  bg-primary-custom flex-grow p-6'>
+    {children}
+  </div>
+);
 
 export default MainLayout;
