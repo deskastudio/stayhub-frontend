be4bff5e90ba6a22c3397283b0c1f02bfc2a963c
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-15 22:38:32 +0800

    add hooks fetchComplaint

--- src/hooks/useFetchComplaint.ts
+++ src/hooks/useFetchComplaint.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { getUserId } from '../utils/auth.utils';
 
 // Interface
@@ -29,56 +29,49 @@ interface Complaint {
   createdAt: string;
 }
 
-interface ComplaintResult {
-  user: User;
-  complaint: Complaint | null;
-  images: string[];
-  loading: boolean;
-  fetchComplaint: () => void;
-}
-
-export const useFetchComplaint = (): ComplaintResult => {
-  const [complaint, setComplaint] = useState<Complaint | null>(null);
+export const useFetchComplaint = () => {
+  const [complaint, setComplaint] = useState<Complaint[] | null>(null);
   const [user, setUser] = useState<User | null>(null);
   const [images, setImage] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
   const id = getUserId();
 
-  const fetchComplaint = async () => {
-    const token = sessionStorage.getItem('token');
-    setLoading(true);
-
-    try {
-      const response = await axios.get(
-        `http://localhost:8000/complaint/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          withCredentials: true,
-        }
-      );
-
-      const data = response.data.data;
-      console.log('data:', data);
-      if (data.length > 0) {
-        setComplaint(data.map((complaint: Complaint) => complaint));
-        setUser(data.user);
-        setImage(data.map((complaint: Complaint) => complaint?.images[0]?.url));
-      } else {
-        setComplaint(null);
-        setUser(null);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
-    fetchComplaint();
-  }, []);
+    const fetchComplaint = async () => {
+      const token = sessionStorage.getItem('token');
+      setLoading(true);
 
-  return { fetchComplaint, complaint, user: user as User, images, loading };
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/complaint/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            withCredentials: true,
+          }
+        );
+
+        const data = response.data.data;
+        if (data.length > 0) {
+          setComplaint(data.map((complaint: Complaint) => complaint));
+          setUser(data.user);
+          setImage(
+            data.map((complaint: Complaint) => complaint?.images[0]?.url)
+          );
+        } else {
+          setComplaint(null);
+          setUser(null);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchComplaint();
+  }, [id]);
+
+  return { complaint, user, images, loading };
 };
