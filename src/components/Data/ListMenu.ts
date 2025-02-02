2d4149e5028f4f39afa93fc294eaa23b0abd45fb
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-02-03 07:26:32 +0800

    add ListMenu component for navigation links and localization

---
+++ src/components/Data/ListMenu.ts
@@ -0,0 +1,24 @@
+const ListMenu = [
+  {
+    id: 1,
+    title: 'Beranda',
+    link: '/',
+  },
+  {
+    id: 2,
+    title: 'Tentang Kami',
+    link: '#tentang-kami',
+  },
+  {
+    id: 3,
+    title: 'Tipe Kamar',
+    link: '#tipe-kamar',
+  },
+  {
+    id: 4,
+    title: 'Kontak',
+    link: '#kontak',
+  },
+];
+
+export default ListMenu;
