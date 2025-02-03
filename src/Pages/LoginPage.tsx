148cee4799d5bc79d912f3b75cc9642b246c70b0
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-02-03 11:23:05 +0800

    refactor LoginPage: update class names to use single quotes for consistency

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -3,13 +3,13 @@ import ImageCarousel from '../components/Fragments/ImageCarousel';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center bg-white">
-      <div className="grid grid-cols-1 md:grid-cols-2 h-full w-full bg-white">
-        <div className="">
+    <div className='min-h-screen flex justify-center items-center bg-white'>
+      <div className='grid grid-cols-1 md:grid-cols-2 h-full w-full bg-white'>
+        <div>
           <ImageCarousel />
         </div>
-        <div className="flex items-center justify-center">
-          <div className="w-full max-w-md px-6">
+        <div className='flex items-center justify-center'>
+          <div className='w-full max-w-md px-6'>
             <LoginForm />
           </div>
         </div>
